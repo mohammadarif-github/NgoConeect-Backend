c4f8f5dbf92c050cdf421b36d7f2a634bfbaedf1
ariful-git-hub <ariful@lexaeon.com>
2025-11-16 13:11:57 +0600

    code updated

--- ngoconnect/user/models.py
+++ ngoconnect/user/models.py
@@ -1,5 +1,6 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.db import models
+
 
 class UserManager(BaseUserManager):
     def create_user(self, email, password=None, **extra_fields):
