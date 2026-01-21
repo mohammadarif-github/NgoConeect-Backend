7b1f8088f1d8606d02899ee08dae814018f80e47
Mohammad Arif <mohammadarif.it159@gmail.com>
2026-01-21 16:55:55 +0600

    user registration bug fixed

--- ngoconnect/volunteers/apps.py
+++ ngoconnect/volunteers/apps.py
@@ -4,3 +4,7 @@ from django.apps import AppConfig
 class VolunteersConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'volunteers'
+
+    def ready(self):
+        import volunteers.signals
+
